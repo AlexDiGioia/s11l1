0b7b32fae01f4d6403bbdd4dcbc32ada30e75ad5
AlexDiGioia <digioia.alex2001@gmail.com>
2024-07-08 16:47:50 +0200

    Initial commit

---
+++ src/reducers/index.js
@@ -0,0 +1,13 @@
+const initialState={
+    main:{
+        count:0,
+    },
+}
+
+const mainReducer = (state= initialState, action)=>{
+    switch(action.type){
+        default:
+            return state;
+    }
+}
+export default mainReducer;
